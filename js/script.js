3e6c3317dc5de9955824dd4349bc33514a43aa90
Borovskyi93 <godofgrime@gmail.com>
2023-09-27 10:56:25 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const userHour = +prompt('Enter number of hours:');
 
 if (userHour) {
-    const amountOfSeconds = BigInt(userHour * 3600);
+    const amountOfSeconds = userHour * 3600;
     alert(amountOfSeconds);
 } else {
     alert('INVALID DATA');
